4795439056943eeae1b8764a27d02b1b2a58443c
Nikola Posa <posa.nikola@gmail.com>
2015-09-01 23:28:41 +0200

    Code cleanup.

--- src/NumConverter/DecimalRomanConverter.php
+++ src/NumConverter/DecimalRomanConverter.php
@@ -16,14 +16,14 @@ final class DecimalRomanConverter extends BaseConverter
      * @var array
      */
     private $basicNumbers = array(
-		1    => 'I',
-		5    => 'V',
-		10   => 'X',
-		50   => 'L',
-		100  => 'C',
-		500  => 'D',
-		1000 => 'M'
-	);
+        1 => 'I',
+        5 => 'V',
+        10 => 'X',
+        50 => 'L',
+        100 => 'C',
+        500 => 'D',
+        1000 => 'M'
+    );
 
     public function getSupportedSystems()
     {
@@ -50,29 +50,29 @@ final class DecimalRomanConverter extends BaseConverter
     }
 
     /**
-	 * @param mixed $number
-	 * @return string
-	 */
+     * @param mixed $number
+     * @return string
+     */
     private function dec2roman($number)
-	{
+    {
         if (!is_numeric($number) && !ctype_digit($number)) {
             throw new InvalidArgumentException("Non-decimal number supplied for conversion to Roman numeral system");
         }
 
         $number = (int) $number;
 
-		if ($number >= 4000) {
-			throw new RuntimeException('Numbers greater or equal to 4000 are not supported');
-		}
+        if ($number >= 4000) {
+            throw new RuntimeException('Numbers greater or equal to 4000 are not supported');
+        }
 
-		if (array_key_exists($number, $this->basicNumbers)) {
-			return $this->basicNumbers[$number];
-		}
+        if (array_key_exists($number, $this->basicNumbers)) {
+            return $this->basicNumbers[$number];
+        }
 
-		$romanNumber = '';
+        $romanNumber = '';
 
-		$numberString = (string) $number;
-		$numberLength = strlen($numberString);
+        $numberString = (string) $number;
+        $numberLength = strlen($numberString);
 
         for ($i = 0; $i < $numberLength; $i++) {
             $currentNumber = (int) $numberString[$i];
@@ -86,15 +86,15 @@ final class DecimalRomanConverter extends BaseConverter
                 //4 and 9 are written using their higher base (5 or 10).
                 case 4 :
                 case 9 :
-                    $romanNumber .= $this->basicNumbers[$decPow] . $this->basicNumbers[($currentNumber+1)*$decPow];
+                    $romanNumber .= $this->basicNumbers[$decPow] . $this->basicNumbers[($currentNumber + 1) * $decPow];
                     break;
                 case 6 :
                 case 7 :
                 case 8 :
-                    $romanNumber .= $this->basicNumbers[5*$decPow] . str_repeat($this->basicNumbers[$decPow], ($currentNumber-5));
+                    $romanNumber .= $this->basicNumbers[5 * $decPow] . str_repeat($this->basicNumbers[$decPow], ($currentNumber - 5));
                     break;
                 case 5 :
-                    $romanNumber .= $this->basicNumbers[5*$decPow];
+                    $romanNumber .= $this->basicNumbers[5 * $decPow];
                     break;
                 default :
                     $romanNumber .= str_repeat($this->basicNumbers[$decPow], $currentNumber);
@@ -102,26 +102,26 @@ final class DecimalRomanConverter extends BaseConverter
             }
         }
 
-		return $romanNumber;
-	}
+        return $romanNumber;
+    }
 
-	/**
-	 * @param mixed $number
-	 * @return int
-	 */
-	private function roman2dec($number)
-	{
+    /**
+     * @param mixed $number
+     * @return int
+     */
+    private function roman2dec($number)
+    {
         $number = (string) $number;
 
-		$number = trim($number);
+        $number = trim($number);
 
         if (!preg_match('/^[' . implode('', array_values($this->basicNumbers)) . ']+$/i', $number)) {
-			throw new InvalidArgumentException("Supplied Roman numeral: '$number' is not valid");
+            throw new InvalidArgumentException("Supplied Roman numeral: '$number' is not valid");
         }
 
-		$number = strtoupper($number);
+        $number = strtoupper($number);
 
-		$decNum = 0;
+        $decNum = 0;
 
         if (($decNum = array_search($number, $this->basicNumbers)) !== false) {
             return $decNum;
@@ -131,10 +131,10 @@ final class DecimalRomanConverter extends BaseConverter
 
         $subtract = false;
 
-        for ($i = strlen($number)-1; $i >=0; $i--) {
+        for ($i = strlen($number) - 1; $i >= 0; $i--) {
             $dn = $basicNumbers[$number[$i]];
 
-            $prevPos = $i+1;
+            $prevPos = $i + 1;
 
             if (isset($number[$prevPos])) {
                 $prevDn = $basicNumbers[$number[$prevPos]];
@@ -153,6 +153,6 @@ final class DecimalRomanConverter extends BaseConverter
             }
         }
 
-		return $decNum;
-	}
+        return $decNum;
+    }
 }
