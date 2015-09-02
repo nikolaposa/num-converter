3619a589a10dbaa3f3948fa4830a57a10c082c6a
Nikola Posa <posa.nikola@gmail.com>
2015-09-02 11:24:37 +0200

    License information.

--- src/Converter.php
+++ src/Converter.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the NumConverter package.
+ *
+ * Copyright (c) Nikola Posa <posa.nikola@gmail.com>
+ *
+ * For full copyright and license information, please refer to the LICENSE file,
+ * located at the package root folder.
+ */
+
 namespace NumConverter;
 
 use NumConverter\Exception\UnsupportedNumeralSystemException;
